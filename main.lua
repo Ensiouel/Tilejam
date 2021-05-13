dc821c3d21e3db944f0633ae5eda8c51339d8e21
Ensiouel <ensiouel@mail.ru>
2021-05-13 12:43:27 +0500

    update

--- main.lua
+++ main.lua
@@ -1,14 +1,15 @@
 local tilejam = require "tilejam"
 
+local map = tilejam:newTilemap("map.lua")
+
 function love.load()
-  local map = tilejam:newTilemap("map.lua")
 
 end
 
 function love.update(dt)
-  map:update(dt)
+  -- map:update(dt)
 end
 
 function love.draw()
-  map:draw()
+  -- map:draw()
 end
--- tilejam/init.lua
+++ tilejam/init.lua
@@ -9,13 +9,19 @@ local ld = love.data
 local Tilejam  = { 
   _URL         = "https://github.com/Ensiouel/Tilejam",
   _VERSION     = "1.0",
-  _DESCRIPTION = "Tilemap library for LÖVE"
+  _DESCRIPTION = "Tilemap library for LÖVE",
+  imagecache = {}
 }
+
+local Tileset = {}
+local Tilemap = {}
+local Grid    = {}
+local Layer   = {}
+
 function Tilejam:new()
   local tilejam      = {}
   tilejam.conf       = {}
   tilejam.conf.debug = true
-  tilejam.imagecache = {}
   setmetatable(tilejam, self)
   self.__index = self; return tilejam
 end
@@ -29,20 +35,39 @@ end
 -- ===========================================================================
 
 function Tilejam:newTilemap( path ) --> Tilemap
+  local map, error, tilesets, tilemap, tileset
 
-end
+  -- Load a lua file with the Tiled data 
+  map, error = love.filesystem.load( path )
+  if error == nil then
+    map = map()
+  else
+    self:debug( error )
+  end
 
--- ===========================================================================
+  tilesets = map.tilesets
+  tilemap = Tilemap:new()
 
-function Tilejam:debug(message) --> nil
-  if self.conf.debug then
-    print(message)
+  for k, v in pairs( tilesets ) do
+    tilemap:newTileset( v )
+  end
+
+  for k, v in pairs( tilemap.tilesets ) do
+    print(k, v)
   end
 end
 
 -- ===========================================================================
 
-function Tilejam:newTiled(path)
+function Tilejam:debug( ... ) --> nil
+  if self.conf.debug then
+    print( ... )
+  end
+end
+
+-- ===========================================================================
+
+function Tilejam:newTiled( path )
 
 end
 
@@ -52,27 +77,27 @@ function Tilejam:newImage( path ) --> Image
   local hashPath = ld.hash( "md5", path )
 
   -- Search by image path
-  if self.imagecache[hashPath] ~= nil then
-    return self.imagecache[hashPath].image
+  if Tilejam.imagecache[hashPath] ~= nil then
+    return Tilejam.imagecache[hashPath].image
   end
 
   local imageData = li.newImageData( path )
   local hashImage = ld.hash( "md5", imageData )
 
   -- Search by image content
-  for k, image in pairs(self.imagecache) do
+  for k, image in pairs( Tilejam.imagecache ) do
     if image.hashImage == hashImage then
-      return self.imagecache[k].image
+      return Tilejam.imagecache[k].image
     end
   end
 
   -- Creating an image cache
-  self.imagecache[hashPath] = {
+  Tilejam.imagecache[hashPath] = {
     hashImage = hashImage,
-    image = lg.newImage(imageData)
+    image = lg.newImage( imageData )
   }
 
-  return self.imagecache[hashPath].image
+  return Tilejam.imagecache[hashPath].image
 end
 
 -- ===========================================================================
@@ -85,8 +110,7 @@ end
 -- Tilemap
 -- ===========================================================================
 
-local Tilemap = {}
-function Tilemap:new(params)
+function Tilemap:new( params )
   local tilemap    = {}
   tilemap.tilesets = {} --[[ name, Tileset ]]
   tilemap.layers   = {} --[[ id,   Layer   ]]
@@ -94,30 +118,31 @@ function Tilemap:new(params)
   self.__index = self; return tilemap
 end
 
-function Tilemap:newLayer(params) --> Layer
-  local layer = Layer:new(params)
+function Tilemap:newLayer( params ) --> Layer
+  local layer = Layer:new( params )
   if self.layers[layer.name] == nil then
     self.layers[layer.name] = layer
     return self.layers[layer.name]
   end
 end
 
-function Tilemap:newTileset(params) --> Tileset
-  local tileset = Tileset:new(params)
+function Tilemap:newTileset( params ) --> Tileset
+  local tileset = Tileset:new( params )
   if self.tilesets[tileset.name] == nil then
     self.tilesets[tileset.name] = tileset
     return self.tilesets[tileset.name]
   end
+  return nil
 end
 
-function Tilemap:update(dt)
-  for k, layer in pairs(self.layers) do
+function Tilemap:update( dt )
+  for k, layer in pairs( self.layers ) do
 
   end
 end
 
 function Tilemap:draw()
-  for k, layer in pairs(self.layers) do
+  for k, layer in pairs( self.layers ) do
 
   end
 end
@@ -126,18 +151,17 @@ end
 -- Tileset
 -- ===========================================================================
 
-local Tileset = {}
-function Tileset:new(params)
+function Tileset:new( params )
   local tileset              = {}
   tileset.name               = params.name
   tileset.firstgid           = params.firstgid
   tileset.tile               = {}
-    tileset.tile.width       = params.tile.width
-    tileset.tile.height      = params.tile.height
-    tileset.tile.сount       = params.tile.сount
+    tileset.tile.width       = params.tilewidth
+    tileset.tile.height      = params.tileheight
+    tileset.tile.сount       = params.tilecount
     tileset.tile.offset      = {}
-      tileset.tile.offset.x  = params.tile.offset.x
-      tileset.tile.offset.y  = params.tile.offset.y
+      tileset.tile.offset.x  = params.tileoffset.x
+      tileset.tile.offset.y  = params.tileoffset.y
   tileset.grid               = {}
     tileset.grid.orientation = params.grid.orientation
     tileset.grid.width       = params.grid.width
@@ -145,10 +169,10 @@ function Tileset:new(params)
   tileset.spacing            = params.spacing
   tileset.margin             = params.margin
   tileset.columns            = params.columns
-  tileset.image              = params.image
+  tileset.image              = Tilejam:newImage(params.image)
   tileset.imageWidth         = tileset.image:getWidth()
   tileset.imageHeight        = tileset.image:getHeight()
-  tileset.objectAlignment    = params.objectAlignment
+  tileset.objectAlignment    = params.objectalignment
   tileset.properties         = params.properties
   tileset.tiles              = params.tiles
   setmetatable(tileset, self)
@@ -159,7 +183,6 @@ end
 -- Layer
 -- ===========================================================================
 
-local Layer        = {}
 function Layer:new()
   local layer      = {}
   layer.id         = -1
@@ -181,7 +204,6 @@ end
 -- Grid
 -- ===========================================================================
 
-local Grid        = {}
 function Grid:new()
   local grid      = {}
   setmetatable(grid, self)
